4237ca70542c6e68d7f099f31727aa169426441b
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-16 16:06:37 -0500

    form for generating posts

--- pages/post/new.js
+++ pages/post/new.js
@@ -36,20 +36,60 @@ export default function NewPost(props) {
             console.log('Error: ', error);
         }
     };
-    const handleClick = async () => {
-        const response = await fetch('/api/generatePost', {
-            method: 'POST',
-        });
-        const data = await response.json();
-        console.log('Data: ', data);
-    };
 
     return (
-        <div>
-            <h1>This is the New Post page</h1>
-            <button className="btn" onClick={handleClick}>
-                Generate Post
-            </button>
+        <div className="h-full overflow-hidden">
+            {!!generating && (
+                <div className="color-main flex h-full animate-pulse w-full flex-col justify-center items-center">
+                    <FontAwesomeIcon icon={faBrain} className="text-8xl" />
+                    <h6>Generating...</h6>
+                </div>
+            )}
+            {!generating && (
+                <div className="w-full h-full flex flex-col overflow-auto">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
+                    >
+                        <div>
+                            <label>
+                                <strong>
+                                    Generate a blog post on the topic of:
+                                </strong>
+                            </label>
+                            <textarea
+                                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                                value={topic}
+                                onChange={(e) => setPostTopic(e.target.value)}
+                                maxLength={80}
+                            />
+                        </div>
+                        <div>
+                            <label>
+                                <strong>
+                                    Enter the SEO keywords you want to target:
+                                </strong>
+                            </label>
+                            <textarea
+                                className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"
+                                value={seoKeywords}
+                                onChange={(e) => setSeoKeywords(e.target.value)}
+                                maxLength={80}
+                            />
+                            <small className="block mb-2">
+                                *Make sure to separate each keyword with a comma
+                            </small>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn"
+                            disabled={!postTopic.trim() || !seoKeywords.trim()}
+                        >
+                            Generate
+                        </button>
+                    </form>
+                </div>
+            )}
         </div>
     );
 }
@@ -58,10 +98,21 @@ NewPost.getLayout = function getLayout(page, pageProps) {
     return <AppLayout {...pageProps}>{page}</AppLayout>;
 };
 
-export const getServerSideProps = withPageAuthRequired(() => {
-    return {
-        props: {
-            test: 'this is a test',
-        },
-    };
+export const getServerSideProps = withPageAuthRequired({
+    async getServerSideProps(ctx) {
+        const props = await getAppProps(ctx);
+
+        if (!props.availableTokens) {
+            return {
+                redirect: {
+                    destination: '/token-purchase',
+                    permanent: false,
+                },
+            };
+        }
+
+        return {
+            props,
+        };
+    },
 });
