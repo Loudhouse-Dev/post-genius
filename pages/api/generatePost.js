0b340d28418758828d0c38ef6cecda543e09b3ff
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-16 15:57:55 -0500

    generate post functionality and UI

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,3 +1,147 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: 'John Doe' });
-}
+import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { Configuration, OpenAIApi } from 'openai';
+import clientPromise from '../../lib/mongodb';
+
+export default withApiAuthRequired(async function handler(req, res) {
+    const { user } = await getSession(req, res);
+    const client = await clientPromise;
+    const db = client.db('BlogStandard');
+    const userProfile = await db.collection('users').findOne({
+        auth0Id: user.sub,
+    });
+
+    if (!userProfile?.availableTokens) {
+        res.status(403);
+        return;
+    }
+
+    const config = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(config);
+
+    const { topic, keywords } = req.body;
+
+    if (!topic || !keywords) {
+        res.status(422);
+        return;
+    }
+
+    if (topic.length > 80 || keywords.length > 80) {
+        res.status(422);
+        return;
+    }
+
+    //gpt-3.5-turbo model endpoint is createChatCompletion
+    //we are going to use the model to generate posts by giving it a series of prompts with different roles
+    const postContentResult = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                //There are 3 roles: system, user, and assistant
+                role: 'system',
+                //here we are using the system role to tell the model what it is
+                content: 'You are a blog post generator.',
+            },
+            {
+                //here, we are using the user role to tell the model what we want it to do
+                role: 'user',
+                content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+            },
+        ],
+        temperature: 0,
+    });
+
+    //the model will return a response with the content of the blog post
+    //save the outputted content to a variable
+    //we will use this variable to generate the title and meta description
+    const postContent = postContentResult.data.choices[0]?.message.content;
+
+    //we do the same here to generate a SEO friendly title tag
+    const titleResult = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: 'You are a blog post generator.',
+            },
+            {
+                role: 'user',
+                content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+            },
+            {
+                role: 'assistant',
+                content: postContent,
+            },
+            {
+                role: 'user',
+                content:
+                    'Generate appropriate title tag text for the above blog post',
+            },
+        ],
+        temperature: 0,
+    });
+
+    const metaDescriptionResult = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: 'You are a blog post generator.',
+            },
+            {
+                role: 'user',
+                content: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}. 
+      The response should be formatted in SEO-friendly HTML, 
+      limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, i, ul, li, ol.`,
+            },
+            {
+                role: 'assistant',
+                content: postContent,
+            },
+            {
+                role: 'user',
+                content:
+                    'Generate SEO-friendly meta description content for the above blog post',
+            },
+        ],
+        temperature: 0,
+    });
+
+    const title = titleResult.data.choices[0]?.message.content;
+    const metaDescription =
+        metaDescriptionResult.data.choices[0]?.message.content;
+
+    console.log('POST CONTENT: ', postContent);
+    console.log('TITLE: ', title);
+    console.log('META DESCRIPTION: ', metaDescription);
+
+    /*await db.collection('users').updateOne(
+  {
+    auth0Id: user.sub,
+  },
+  {
+    $inc: {
+      availableTokens: -1,
+    },
+  }
+);*/
+
+    const post = await db.collection('posts').insertOne({
+        postContent: postContent || '',
+        title: title || '',
+        metaDescription: metaDescription || '',
+        topic,
+        keywords,
+        userId: userProfile._id,
+        created: new Date(),
+    });
+
+    res.status(200).json({
+        postId: post.insertedId,
+    });
+});
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,9 +1,41 @@
 import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { faBrain } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { AppLayout } from '../../components/AppLayout';
+import { getAppProps } from '../../utils/getAppProps';
 
 export default function NewPost(props) {
-    console.log('New Post Props: ', props);
+    router = useRouter();
 
+    //state variables for the form
+    const [postTopic, setPostTopic] = useState('');
+    const [seoKeywords, setSeoKeywords] = useState('');
+    const [generating, setGenerating] = useState(false);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setGenerating(true);
+        try {
+            const response = await fetch('/api/generatePost', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify({ postTopic, seoKeywords }),
+            });
+            const data = await response.json();
+            console.log('Data: ', data);
+            setGenerating(false);
+            if (data?.postId) {
+                router.push(`/post/${data.postId}`);
+            }
+        } catch (error) {
+            setGenerating(false);
+            console.log('Error: ', error);
+        }
+    };
     const handleClick = async () => {
         const response = await fetch('/api/generatePost', {
             method: 'POST',
