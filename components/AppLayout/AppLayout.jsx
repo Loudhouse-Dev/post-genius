0f71651bb3f0cab64f1751a741b5f39deafa4dfa
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-16 16:37:36 -0500

    stopping point

--- components/AppLayout/AppLayout.jsx
+++ components/AppLayout/AppLayout.jsx
@@ -1,12 +1,35 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { useContext, useEffect } from 'react';
+import PostContext from '../../context/postContext';
 import { Logo } from '../Logo';
 
-export const AppLayout = ({ children }) => {
+export const AppLayout = ({
+    children,
+    postId,
+    postCreated,
+    posts: postsFromSSR,
+    tokensAvailable,
+}) => {
     const { user, error, isLoading } = useUser();
+
+    const { setPostsFromSSr, posts, getPosts, noMorePosts } =
+        useContext(PostContext);
+
+    useEffect(() => {
+        setPostsFromSSr(postsFromSSR);
+        if (postId) {
+            const postExists = posts.find((post) => post._id === postId);
+            if (!postExists) {
+                getPosts({ getNewestPosts: true, lastPostDate: postCreated });
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [postsFromSSR, setPostsFromSSr, postId, postCreated, getPosts]);
+
     return (
         <div className="grid grid-cols-[400px_1fr] h-screen max-h-screen bg-gray-200 shadow-xl">
             <div className="flex flex-col text-black overflow-hidden border-r border-r-gray-500/40">
@@ -26,11 +49,39 @@ export const AppLayout = ({ children }) => {
                             size="text-sm"
                             className="inline-block text-center text-yellow-500"
                         />
-                        <span className="pl-1"> 0 Tokens </span>
+                        <span className="pl-1">
+                            {' '}
+                            {tokensAvailable} Tokens Left{' '}
+                        </span>
                     </Link>
                 </div>
-                <div className="flex-1 overflow-auto bg-gradient-to-b from-gray-200 to-gray-300 border-b border-b-gray-400/40 ">
-                    Post Listing
+                <div className="px-4 flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
+                    {posts.map((post) => (
+                        <Link
+                            key={post._id}
+                            href={`/post/${post._id}`}
+                            className={`py-1 border border-white/0 block text-ellipsis overflow-hidden whitespace-nowrap my-1 px-2 bg-white/10 cursor-pointer rounded-sm ${
+                                postId === post._id
+                                    ? 'bg-white/20 border-white'
+                                    : ''
+                            }`}
+                        >
+                            {post.topic}
+                        </Link>
+                    ))}
+                    {!noMorePosts && (
+                        <div
+                            onClick={() => {
+                                getPosts({
+                                    lastPostDate:
+                                        posts[posts.length - 1].created,
+                                });
+                            }}
+                            className="hover:underline text-sm text-slate-400 text-center cursor-pointer mt-4"
+                        >
+                            Load more posts
+                        </div>
+                    )}
                 </div>
                 <div className="bg-gray-300 flex items-center gap-3 border-t border-t-gray-200/60 h-40 px-2">
                     {!!user ? (
