1ff42fdb36f71cb34da6b95a0ef03e72d6302d0f
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-16 11:03:56 -0500

    landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
 import '../styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Archivo, Archivo_Black } from '@next/font/google';
+
+const archivo = Archivo({
+    weight: ['100', '200', '300', '400', '500'],
+    subsets: ['latin'],
+    variable: '--font-archivo',
+});
+
+const archivoBlack = Archivo_Black({
+    weight: ['400'],
+    subsets: ['latin'],
+    variable: '--font-archivo-black',
+});
 
 function MyApp({ Component, pageProps }) {
     const getLayout = Component.getLayout || ((page) => page);
     return (
         <UserProvider>
-            {getLayout(<Component {...pageProps} />, pageProps)}
+            <main
+                className={`${archivo.variable} ${archivoBlack.variable} font-body`}
+            >
+                {getLayout(<Component {...pageProps} />, pageProps)}
+            </main>
             <Component {...pageProps} />
         </UserProvider>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 export default function Home() {
     return (
-        <div className="home-screen w-screen h-screen overflow-hidden flex justify-center items-center relative"></div>
+        <div className="home-screen w-screen h-screen overflow-hidden flex justify-center items-center relative">
+            <div className="glass-effect"></div>
+        </div>
     );
 }
